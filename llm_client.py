cb72ed9b6e00a13a01c3a85635a9fce51dfb26c1
evan <evan@leyantech.com>
2024-03-22 14:49:06 +0800

    update

--- llm_client.py
+++ llm_client.py
@@ -1,53 +1,53 @@
 from dotenv import load_dotenv
 
+default_stop = [
+    "<|im_end|>",
+    "</s>",
+    "\n\n\n"
+]
 
-def gpt4():
+
+def gpt4(temperature=0, **kwargs):
     load_dotenv('env/azure.env', verbose=True)
     from langchain_openai import AzureChatOpenAI
+    kwargs.setdefault('seed', 42)
     return AzureChatOpenAI(
         deployment_name="gpt-4",
-        temperature=0,
-        model_kwargs={
-            'seed': 42,
-            'stop': [
-                "<|im_end|>",
-                "</s>",
-                "\n\n\n"
-            ]
-        }
+        temperature=temperature,
+        model_kwargs=kwargs
     )
 
 
-def gpt3_5():
+def gpt3_5(temperature=0, **kwargs):
     load_dotenv('env/azure.env', verbose=True)
     from langchain_openai import AzureChatOpenAI
+    kwargs.setdefault('seed', 42)
     return AzureChatOpenAI(
         deployment_name="gpt-35-turbo",
-        temperature=0,
-        model_kwargs={
-            'seed': 42,
-            'stop': [
-                "<|im_end|>",
-                "</s>",
-                "\n\n\n"
-            ]
-        }
+        temperature=temperature,
+        model_kwargs=kwargs
     )
 
 
-def qwen72b():
+def qwen72b_chat(temperature=0, max_token=300, **kwargs):
     load_dotenv('env/qwen72b.env', verbose=True)
     from langchain_openai import ChatOpenAI
+    kwargs.setdefault('seed', 42)
     return ChatOpenAI(
         model_name="qwen1.5-72b-chat-int4",
-        max_tokens=300,
-        temperature=0,
-        model_kwargs={
-            'seed': 42,
-            'stop': [
-                "<|im_end|>",
-                "</s>",
-                "\n\n\n"
-            ]
-        }
+        max_tokens=max_token,
+        temperature=temperature,
+        model_kwargs=kwargs
+    )
+
+
+def qwen72b(temperature=0, max_token=300, **kwargs):
+    load_dotenv('env/qwen72b.env', verbose=True)
+    from langchain_openai import OpenAI
+    kwargs.setdefault('seed', 42)
+    return OpenAI(
+        model_name="qwen1.5-72b-chat-int4",
+        max_tokens=max_token,
+        temperature=temperature,
+        model_kwargs=kwargs
     )
--- test.py
+++ test.py
@@ -1,4 +1,136 @@
-from dotenv import load_dotenv
+from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from llm_client import *
+
+prompt = PromptTemplate.from_template(
+    "What is a good name for a company that makes {product}?"
+)
+
+output_parser = StrOutputParser()
+
+
+def test_gpt():
+    llm = gpt4()
+    chain = llm | output_parser
+    res = chain.invoke('hi')
+    print(res)
+
+
+def test_chain():
+    llm = qwen72b_chat(temperature=0)
+    chain = LLMChain(
+        prompt=prompt,
+        llm=llm,
+        output_parser=output_parser,
+    )
+
+    res = chain.invoke({'product': 'socks'})
+    print(res)
+
+
+def test_agent():
+    llm = qwen72b_chat(temperature=0, stop=None)
+    tools = load_tools(["llm-math"], llm=llm)
+    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+    res = agent.run("2 raised to the 8 power?")
+    print(res)
+
+
+def test_conversation():
+    from langchain.chains import ConversationChain
+    llm = qwen72b(temperature=0, max_token=20)
+    conversation = ConversationChain(llm=llm, verbose=True)
+    output = conversation.predict(input="Hi there!")
+    print(output)
+    output = conversation.predict(input="enen!")
+    print(output)
+
+
+def test_chat():
+    llm = qwen72b_chat(temperature=0)
+    from langchain_core.prompts import ChatPromptTemplate
+    messages = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful assistant that translates English to Chinese."),
+        ("user", "Translate this sentence from English to Chinese. I love programming.")
+    ])
+    chain = messages | llm
+    res = chain.invoke({})
+    res.pretty_print()
+
+
+def test_python():
+    from langchain_experimental.agents.agent_toolkits import create_python_agent
+    llm = qwen72b(stop=None)
+    from langchain_experimental.tools import PythonREPLTool
+    agent = create_python_agent(
+        llm=llm,
+        tool=PythonREPLTool(),
+        verbose=True
+    )
+    res = agent.invoke({'input': '把"abc"拆封成array'})
+    print(res)
+
+
+def custom_agent():
+    from langchain.agents import Tool
+    from langchain_core.prompts import PromptTemplate
+
+    template = '''
+Answer the following questions as best you can. You have access to the following tools:
+You might know the answer without running any tools, but you should still run the tools to get the answer.
+If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {input}
+Thought:{agent_scratchpad}'''
+
+    prompt = PromptTemplate.from_template(template)
+
+    def add(value: int):
+        print("run add")
+        return int(value) + 1
+
+    tools = [
+        Tool.from_function(add,
+                           name='add',
+                           description=(
+                               "一个计算器工具，用于为给定的数字加1"
+                               "输入是一个int类型的值"
+                           ),
+                           return_direct=False)
+    ]
+
+    from langchain.agents import create_react_agent, AgentExecutor
+    llm = qwen72b()
+    agent = create_react_agent(llm, tools, prompt)
+
+    executor = AgentExecutor.from_agent_and_tools(
+        agent=agent,
+        tools=tools,
+        verbose=True,
+        stream_runnable=False,
+    )
+
+    res = executor.invoke({'input': '给2增加1'}, return_only_outputs=True)
+    print(res)
+
 
 if __name__ == '__main__':
-    load_dotenv('env/azure.env', )
+    test_gpt()
